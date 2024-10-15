a46af5d6c7ca07d260ead0eb9ebdddc46946c053
TharushaK01 <tharushakavinda01@gmail.com>
2024-10-15 18:03:51 +0530

    sign with google done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Pages from './Components/Pages/Pages';
 import { BrowserRouter } from 'react-router-dom';
+import AppContext from './Components/AppContext/AppContext';
 
 function App() {
   return  (  <h1 className="App">
     <BrowserRouter>
+    <AppContext>
   <Pages></Pages>
+  </AppContext>
   </BrowserRouter>
 </h1>
 
