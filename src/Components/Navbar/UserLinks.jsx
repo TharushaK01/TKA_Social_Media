c58adca5f9dcb2fdc4e1bf1e722576d53faa602f
TharushaK01 <tharushakavinda01@gmail.com>
2024-10-17 15:50:02 +0530

    updating data base

--- src/Components/Navbar/UserLinks.jsx
+++ src/Components/Navbar/UserLinks.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 
 
 
 import { Tooltip } from "@material-tailwind/react";
 import { Avatar } from "@material-tailwind/react";
 import avatar from '../../assets/images/avatar.jpg';
+import { AuthContext } from "../AppContext/AppContext";
 
 
 
 const UserLinks = () => {
+
+  const { signOutUser, user, userData, underfined } = useContext(AuthContext);
     return (
     <div className="flex justify-center items-center cursor-pointer">
         <div className="hover:translate-y-1 duration-500 ease-in-out hover:text-blue-500">
@@ -27,13 +30,13 @@ const UserLinks = () => {
   <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
 </svg>
         </diV>
-        <div className="mx-4 flex items-center">
+        <div className="mx-4 flex items-center" onClick={signOutUser}>
         <img className="h-10 " size="sm" src={avatar} alt="avatar"></img>
        {/*<Tooltip content="Sign Out" placement="bottom">
           <Avatar src={avatar} size="sm" alt="avatar" />
         </Tooltip>*/}
             <p className="ml-4 font-roboto text-sm text-black font-medium no-underline ">
-            User
+            {user?.displayName === null && userData?.name !== underfined? userData?.name?.charAt(0)?.toUpperCase() + userData?.name?.slice(1) : user?.displayName?.split("")[0]}
             </p>
         </div>
     </div>
--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -1,18 +1,33 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../AppContext/AppContext';
+import { auth, onAuthStateChanged } from '../firebase/firebase';
+
 
 const Login = () => {
+ 
   const { signInWithGoogle } = useContext(AuthContext);
+  
 
+  {/*useEffect(() => {
+    setLoading(true);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/");
+        setLoading(true);
+      }else {
+        setLoading(false);
+      }
+    }); 
+    }, [navigate]);*/}
 
   return ( 
         <div className='flex justify-center items-center h-screen bg-gray-200'>
           <div className='px-10 py-10  shadow-lg bg-white rounded-md border-b-2 border-gray-300'>
             <h1 className='text-3xl block text-center font-semibold text-transparent bg-clip-text bg-gradient-to-r to-red-500 from-blue-900'> LOGIN </h1>
-            <span>{/* ProgressBar */}</span>
+            <span>{/**/} </span>
             
-           <div class='mt-3 p-6'>
+        <div class='mt-3 p-6'>
            <div className= ' ml-2 mr-2'>
             <label for='email' class='block text-base mb-2 text-lg'>Email</label>
             <input type='text' id='email' className='w-full border rounded-xl text-base px-2 py-1 focus:outline-none focus;ring-0 focus:border-gray-600' placeholder='Enter email'></input>
@@ -40,10 +55,13 @@ const Login = () => {
             <Link to="/">
               <button type='submit' className='border-2 border-indigo-700 bg-indigo-700 font-medium text-white py-1 px-5 w-full rounded-md hover:bg-transparent hover:text-indigo-700'>Login</button>
               </Link>
+              
               <button className='flex border-2 py-1 rounded-md border-gray-200 items-center justify-center gap-2 activ:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all' onClick={signInWithGoogle}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="0.98em" height="1em" viewBox="0 0 256 262"><path fill="#4285f4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"/><path fill="#34a853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"/><path fill="#fbbc05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"/><path fill="#eb4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"/></svg>
                 Sign with Google</button>
+                
             </div>
+
             <div className='mt-3 flex justify-center items-center'>
               <p className='font-medium text-base'>Don't have an account</p>
               <Link to="/Register">
