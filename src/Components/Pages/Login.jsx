882dea3e95679c07e43ba966aeb1276c115a747f
TharushaK01 <tharushakavinda01@gmail.com>
2024-10-29 14:30:23 +0530

    new start

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useContext, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../AppContext/AppContext';
--- src/Components/Pages/Pages.jsx
+++ src/Components/Pages/Pages.jsx
@@ -4,7 +4,6 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import Reset from "./Reset";
-import PostCards from "../Main/PostCards";
 
 
 
